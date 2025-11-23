23aefcb6f8bc6b68f048bbb4734d0b7b99ac16b9
Berk Yeni <141870638+BerkYeni@users.noreply.github.com>
2025-11-23 16:08:16 +0300

    improved the color mask program by adding color selection

--- OpenCV_Tutorial/Image_Processing/Color_Spaces/Changing_Color_Spaces_and_Object_Tracking.py
+++ OpenCV_Tutorial/Image_Processing/Color_Spaces/Changing_Color_Spaces_and_Object_Tracking.py
@@ -13,33 +13,37 @@ def nothing(x):
     pass
 
 
-img = np.zeros((300, 512, 3), np.uint8)
-cv.namedWindow("Image")
-
+# add trackbar to change which color to be tracked
+color_window = "Color"
+color_display_img = np.zeros((300, 512, 3), np.uint8)
+cv.namedWindow(color_window)
+cv.createTrackbar("H", color_window, 0, 179, nothing)
 
 while 1:
 
     # Take each frame
     _, frame = cap.read()
 
-    # Convert BGR to HSV
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    # get hue value from trackbar
+    h = cv.getTrackbarPos("H", color_window)
+    hsv_value = [h, 255, 255]
+    bgr_value = cv.cvtColor(np.uint8([[hsv_value]]), cv.COLOR_HSV2BGR)
 
-    # get green in opencv's hsv scale
-    green = 120 / 360
-    g_in_cv_hsv = green * 179
-    # define range of blue color in HSV
-    lower_blue = np.array([g_in_cv_hsv - 20, 50, 50])
-    upper_blue = np.array([g_in_cv_hsv + 20, 255, 255])
+    # Convert frame from BGR to HSV
+    hsv_img = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    error_margin = 20
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    lower_color_value = np.array([h - error_margin, 50, 50])
+    upper_color_value = np.array([h + error_margin, 255, 255])
+
+    # Threshold the HSV image to get only the selected color
+    mask = cv.inRange(hsv_img, lower_color_value, upper_color_value)
 
     # Bitwise-AND mask and original image
-    # use a solid green image to make the color more apparent
-    solid_green = np.zeros(frame.shape, np.uint8)
-    solid_green[:] = (0, 255, 0)
-    green_img = cv.bitwise_and(solid_green, solid_green, mask=mask)
+    # use a solid color image to make the color more apparent
+    solid_color = np.zeros(frame.shape, np.uint8)
+    solid_color[:] = bgr_value
+    green_img = cv.bitwise_and(solid_color, solid_color, mask=mask)
 
     # get the rest of the img (background) and make it grayscale
     grayed = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -49,6 +53,8 @@ while 1:
     # combine the foreground and background
     fun = cv.add(same_channel_sized_gray, green_img)
 
+    color_display_img[:] = bgr_value
+    cv.imshow(color_window, color_display_img)
     cv.imshow("frame", frame)
     cv.imshow("mask", mask)
     cv.imshow("green_img", green_img)
