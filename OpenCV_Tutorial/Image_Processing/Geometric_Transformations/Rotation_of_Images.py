0a6b007243ece0b19edaeec93204987bdc863ca5
Berk Yeni <141870638+BerkYeni@users.noreply.github.com>
2025-11-23 14:39:24 +0300

    forgot path fix, fixed

--- OpenCV_Tutorial/Image_Processing/Geometric_Transformations/Rotation_of_Images.py
+++ OpenCV_Tutorial/Image_Processing/Geometric_Transformations/Rotation_of_Images.py
@@ -5,7 +5,7 @@ from Utils import show
 # |cos(a)  -sin(a)|
 # |sin(a)   cos(a)|
 
-img = cv.imread("starry_night.jpg", cv.IMREAD_GRAYSCALE)
+img = cv.imread("OpenCV_Tutorial/Resources/starry_night.jpg", cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
 rows, cols = img.shape
 
